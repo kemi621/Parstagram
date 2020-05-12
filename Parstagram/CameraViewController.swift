47ec2c775c41a10c6a55dae3128a47bb9923aee7
kemi621 <miriamakande@yahoo.com>
2020-05-12 14:54:36 -0700

    Added photo feed

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -22,7 +23,26 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground{(success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            }
+            else{
+                print("error")
+            }
+        }
     }
+        
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -42,7 +62,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         let image = info[.editedImage] as! UIImage
         
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
+        let scaledImage = image.af.imageScaled(to: size)
         imageView.image = scaledImage
         
         dismiss(animated: true, completion: nil)
